42d379e522b02828a20a469b6a8691824a624528
KFKing <mehdishakoori1989@gmail.com>
2019-09-06 11:01:28 +0430

    Hub

---
+++ Courier/Assets/AstarPathfindingProject/Core/AI/me/Gate.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Gate : MonoBehaviour
+{
+    public int sceneNumber;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            SceneManager.LoadScene(sceneNumber);
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
